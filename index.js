22aa5abb4bb8b4d5683e074fceaea7849cfaee9d
Mfoniso Benn <mfoniso.ndaobong@gmail.com>
2024-12-12 14:09:47 +0100

    Bug fixes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
-tabBtn.addEventListener("click", function(){    
+tabBtn.addEventListener("click", function() {  
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads) )
