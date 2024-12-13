33f57110807ef5a5160b311bef727a962c89dd54
Mfoniso Benn <mfoniso.ndaobong@gmail.com>
2024-12-13 14:28:55 +0100

    Bug fixes

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-let myLeads = []
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js" 
+import { getDatabase, 
+        ref,
+        push
+} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js" 
+
+const firebaseConfig = {
+    databaseURL: "https://leads-tracker-app-8d0d0-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const  referenceInDB = ref(database, "leads")
+
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
-
-tabBtn.addEventListener("click", function() {  
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
 
 function render(leads) {
     let listItems = ""
@@ -34,14 +32,10 @@ function render(leads) {
 }
 
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+
 })
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    push(referenceInDB,  inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
 })
